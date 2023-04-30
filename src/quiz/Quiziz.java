5497f3d16c5e749ddad5e73f9dd06236a0fdc329
Taras-Neridnyi <t.neridniy@gmail.com>
2023-04-30 19:35:17 +0300

    add calibrateTopic method , update askTopic method

--- src/quiz/Quiziz.java
+++ src/quiz/Quiziz.java
@@ -1,5 +1,6 @@
 package quiz;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 
 /**
@@ -7,9 +8,21 @@ import java.util.Scanner;
  * @version 1.0
  */
 class Quiziz {
+
+    /*
+        Fields
+     */
     private static String username ;
     private static String topic;
+    private static LinkedList<String> topics = new LinkedList<>(){{
+        add("Math");
+        add("Programming");
+        add("football");
+    }};
 
+    /*
+        getters and setters
+     */
     public static String getTopic() {
         return topic;
     }
@@ -26,10 +39,14 @@ class Quiziz {
         Quiziz.username = name;
     }
 
+    /*
+        other methods
+     */
+
     protected static void run(){
         askUsername();
         greet();
-        chooseTopic();
+        askTopic();
 
     }
 
@@ -41,14 +58,40 @@ class Quiziz {
     }
 
     private static void greet(){
-        Quiziz quiziz = new Quiziz();
         System.out.println("Привіт " + getName() +  """
                  Це Quizizoo гра !
                 """);
     }
 
-    private static String chooseTopic(){
-        return "";
+    private static void askTopic(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("""
+                ok , тепер виберу одну з трьох тем для квізу: 
+                1 - Математика
+                2 - Футбол
+                3 - Програмування
+                    Для вибору можеш писати цифру або любе з цих слів 
+                """);
+        String topic = scanner.nextLine();
+        setTopic(topic);
+        calibrateTopic();
+    }
+
+    private static void calibrateTopic(){
+        String calibratedName = getTopic().strip().toLowerCase();
+        if(calibratedName.contentEquals("1") || calibratedName.contentEquals("математика")){
+            System.out.println("+");
+            setTopic(topics.get(0));
+        }else if(calibratedName.contentEquals("2") || calibratedName.contentEquals("футбол")){
+            System.out.println("++");
+          setTopic(topics.get(1));
+        }else if(calibratedName.contentEquals("3") || calibratedName.contentEquals("Програмування")){
+            System.out.println("+++");
+            setTopic(topics.get(2));
+        }else {
+            System.out.println("error in calibrate topic");;
+        }
+
     }
 
 }
