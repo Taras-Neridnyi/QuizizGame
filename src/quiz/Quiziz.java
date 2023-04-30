5c101495c4910e7126bde4e496dae82d9420e0b0
Taras-Neridnyi <t.neridniy@gmail.com>
2023-04-30 19:11:12 +0300

    readd getters and setters for topic,username fields

--- src/quiz/Quiziz.java
+++ src/quiz/Quiziz.java
@@ -7,6 +7,24 @@ import java.util.Scanner;
  * @version 1.0
  */
 class Quiziz {
+    private static String username ;
+    private static String topic;
+
+    public static String getTopic() {
+        return topic;
+    }
+
+    public static void setTopic(String topic) {
+        Quiziz.topic = topic;
+    }
+
+    public static String getName() {
+        return username;
+    }
+
+    public static void setName(String name) {
+        Quiziz.username = name;
+    }
 
     protected static void run(){
         askUsername();
@@ -15,19 +33,17 @@ class Quiziz {
 
     }
 
-    private static String askUsername(){
-        Quiziz quiziz = new Quiziz();
+    private static void askUsername(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Перед тим як почати , напиши своє ім'я :  ");
         String name = scanner.nextLine();
-
-
-        return name;
+        setName(name);
     }
+
     private static void greet(){
         Quiziz quiziz = new Quiziz();
-        System.out.println("Привіт " +   """
-                Це Quizizoo гра !
+        System.out.println("Привіт " + getName() +  """
+                 Це Quizizoo гра !
                 """);
     }
 
