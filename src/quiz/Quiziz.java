8db69265e233ed9f6e9cde3988c5db694ff8e2bc
Taras-Neridnyi <t.neridniy@gmail.com>
2023-04-30 19:01:56 +0300

    update Quiziz class(delete unused code)

--- src/quiz/Quiziz.java
+++ src/quiz/Quiziz.java
@@ -6,40 +6,4 @@ package quiz;
  */
 class Quiziz {
 
-    /*
-        Fields
-     */
-    private Character topic;
-    private String name;
-
-    /*
-        Constructors
-     */
-    public Quiziz(Character topic, String name) {
-        this.topic = topic;
-        this.name = name;
-    }
-
-    public Quiziz() {
-    }
-
-    public Character getTopic() {
-        return topic;
-    }
-
-    /*
-        Getters and setters
-     */
-    public void setTopic(Character topic) {
-        this.topic = topic;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
 }
