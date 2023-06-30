df9ecbe5bfae5dd3e59107ed4e62d517552f7105
tarasneridnyi <t.neridniy@gmail.com>
2023-06-30 11:02:48 +0300

    create gets and sets

--- src/quiz/Question.java
+++ src/quiz/Question.java
@@ -2,4 +2,22 @@ package quiz;
 
 public class Question {
 
+    private String answer;
+    private String question;
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
 }
--- src/quiz/Topic.java
+++ src/quiz/Topic.java
@@ -2,4 +2,13 @@ package quiz;
 
 public class Topic {
 
+    private String title ;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
