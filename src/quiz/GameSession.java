32a03c421699774c1418263d3bd2dbb909b44dca
tarasneridnyi <t.neridniy@gmail.com>
2023-06-30 12:51:56 +0300

    create gets and sets  in GameSession class

--- src/quiz/GameSession.java
+++ src/quiz/GameSession.java
@@ -1,4 +1,31 @@
 package quiz;
 
 public class GameSession {
+    private int score;
+    private String playerName;
+    private String selectedTopic;
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public String getSelectedTopic() {
+        return selectedTopic;
+    }
+
+    public void setSelectedTopic(String selectedTopic) {
+        this.selectedTopic = selectedTopic;
+    }
 }
